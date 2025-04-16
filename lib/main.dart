38baf8247ae20f27511f9ddbed8a70191aef6ca0
memahin <me.mahin1@gmail.com>
2025-04-16 06:20:09 +0600

    start with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp());
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello Flutter!'),
+        ),
+      ),
+    ),
+  );
 }
-
